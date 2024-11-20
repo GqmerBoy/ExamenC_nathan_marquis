6e5cd5ba5a7139b6ee7dec8896850593a190faf4
GqmerBoy <143737643+GqmerBoy@users.noreply.github.com>
2024-11-20 15:00:19 -0500

    Fin de la conception de la scène d'accueil

--- Assets/Scripts/SceneManagement.cs
+++ Assets/Scripts/SceneManagement.cs
@@ -1,18 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class SceneManagement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    //Methode pour changer de scene enutilisant le bouton dans la scene "ExamenC_01"
 
-    // Update is called once per frame
-    void Update()
+    public void GoToNextScene()
     {
-        
+        SceneManager.LoadScene("ExamenC_02");
     }
 }
